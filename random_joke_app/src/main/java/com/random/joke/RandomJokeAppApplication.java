f8037a6eaf9f1f1ca05f2db6ee2df3a5de083bc2
Subham <subham7031979@gmail.com>
2024-07-06 16:35:17 +0530

    first commit

---
+++ random_joke_app/src/main/java/com/random/joke/RandomJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.random.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomJokeAppApplication.class, args);
+	}
+
+}
---
+++ random_joke_app/src/test/java/com/random/joke/RandomJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.random.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
